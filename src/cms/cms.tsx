481c8651be82dd752d2a4dfef964f49d214efaa2
Hiroshi Muraoka <h.muraoka714@gmail.com>
2019-03-21 01:56:01 +0900

    check content on netlify

---
+++ src/cms/cms.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import CMS from 'netlify-cms'
+import BlogPostTemplate from '../components/BlogPostTemplate'
+
+const Preview = ({ entry, widgetFor }) => {
+    console.log(widgetFor('body'))
+    return (
+        <BlogPostTemplate
+            content={widgetFor('body')}
+            description={entry.getIn(['data', 'description'])}
+            tags={entry.getIn(['data', 'tags'])}
+            title={entry.getIn(['data', 'title'])}
+            isPreview={true}
+        />
+    )
+}
+
+CMS.registerPreviewTemplate('blog', Preview)
