a278c079b8b298cffefcfa0ef968ce3d40376405
Hiroshi Muraoka <h.muraoka714@gmail.com>
2019-03-21 17:25:04 +0900

    refactoring

--- src/cms/cms.tsx
+++ src/cms/cms.tsx
@@ -2,40 +2,6 @@ import * as React from 'react'
 import CMS from 'netlify-cms'
 import BlogPostTemplate from '../components/BlogPostTemplate'
 
-// import { kebabCase } from 'lodash'
-
-// const HTMLContent = ({ content, className }) => (
-//     <div className={className} dangerouslySetInnerHTML={{ __html: content }} />
-// )
-
-// const Content = ({ content, className }) => (
-//     <div className={className}>{content}</div>
-// )
-
-// const BlogPostTemplate = ({
-//     content,
-//     contentComponent,
-//     description,
-//     tags,
-//     title,
-//     helmet,
-// }) => {
-//     const PostContent = contentComponent || Content
-
-//     return (
-//         <section className="section">
-//             {helmet || ''}
-//             <div className="container content">
-//                 <div className="columns">
-//                     <div className="column is-10 is-offset-1">
-//                         <PostContent content={content} />
-//                     </div>
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// }
-
 const Preview = ({ entry, widgetFor }) => {
     return (
         <BlogPostTemplate
@@ -49,14 +15,3 @@ const Preview = ({ entry, widgetFor }) => {
 }
 
 CMS.registerPreviewTemplate('blog', Preview)
-
-// const BlogPostPreview = ({ entry, widgetFor }) => (
-//     <BlogPostTemplate
-//         content={widgetFor('body')}
-//         description={entry.getIn(['data', 'description'])}
-//         tags={entry.getIn(['data', 'tags'])}
-//         title={entry.getIn(['data', 'title'])}
-//     />
-// )
-
-// CMS.registerPreviewTemplate('blog', BlogPostPreview)
